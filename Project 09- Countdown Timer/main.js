69e3a3240bee6ce623887a7fbc7d8ff59ab0aa3d
SanikaSajwani <sms.sanika@gmail.com>
2024-09-29 16:57:12 +0500

    Sixth Commit

---
+++ Project 09- Countdown Timer/main.js
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+import { differenceInSeconds } from "date-fns";
+const res = await inquirer.prompt({
+    name: "userInput",
+    type: "number",
+    message: "Enter the count in seconds: ",
+});
+let input = res.userInput;
+function startTime(val) {
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime);
+    // Function to display the remaining time
+    function displayRemainingTime() {
+        const currTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currTime);
+        if (timeDiff <= 0) {
+            console.log("Timer has expired");
+            process.exit(); // Stop the process
+        }
+        const min = Math.floor(timeDiff % (3600 * 24) / 60); // Changed from 3600 to 60 for minutes
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")} mins ${sec.toString().padStart(2, "0")} secs`);
+    }
+    // Run the first display immediately
+    displayRemainingTime();
+    // Start the interval for subsequent updates
+    setInterval(() => {
+        displayRemainingTime();
+    }, 1000);
+}
+startTime(input);
